faa486e1df19699d30bbc28a86c8459c274b5f4a
kiki-kanri <a470666@gmail.com>
2025-12-06 13:43:15 +0800

    chore: remove graceful exit plugin

--- server/plugins/00.graceful-exit.ts
+++
@@ -1,8 +0,0 @@
-function gracefulExit() {
-    process.exit(0);
-}
-
-export default defineNitroPlugin(() => {
-    process.once('SIGINT', gracefulExit);
-    process.once('SIGTERM', gracefulExit);
-});
