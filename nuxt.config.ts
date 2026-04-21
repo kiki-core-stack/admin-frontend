4cbab8261fd7ffa399eabf9b89b4d1b9a430ebc9
kiki-kanri <a470666@gmail.com>
2026-04-21 16:17:57 +0800

    chore: update dependencies and modify font loading method

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,11 +30,22 @@ export default defineNuxtConfig({
             contentSecurityPolicy: {
                 'script-src-attr': [
                     `'unsafe-hashes'`,
-                    `'sha256-nNExX8dGhf3ce7nlLMW210YbT1+ATSaoMpg5lf/l+Ng='`,
+                    `'sha256-F1noxsLOnJhyRSgc0zu5JgzoLjG2BBMaXaSG24k2mRM='`,
                 ],
             },
         },
     },
     ssr: true,
+    unfonts: {
+        google: {
+            families: [
+                // {
+                //     name: 'Noto+Sans+TC',
+                //     styles: 'wght@100..900&display=swap',
+                // },
+            ],
+        },
+        inlineFontFace: false,
+    },
     vite: { server: { allowedHosts: (process.env.DEV_VITE_SERVER_ALLOWED_HOSTS || '').split(',') } },
 });
