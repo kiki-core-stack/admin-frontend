d7ca6d0ae2763f7efae7d2c554b0becbbfeff10c
kiki-kanri <a470666@gmail.com>
2026-07-10 15:11:01 +0800

    fix(lint): preserve micromatch permission semantics

--- app/utils/micromatch.ts
+++ app/utils/micromatch.ts
@@ -1,5 +1,7 @@
-// @ts-expect-error Ignore this error.
+/* eslint-disable e18e/ban-dependencies -- node:path.matchesGlob is not behavior-compatible with micromatch. */
+// @ts-expect-error The browser-compatible package does not provide TypeScript declarations.
 import _micromatch from '@kikiutils/micromatch';
 import type micromatchType from 'micromatch';
+/* eslint-enable e18e/ban-dependencies */
 
 export const micromatch = _micromatch as typeof micromatchType;
